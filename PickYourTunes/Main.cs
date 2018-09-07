a558b14129d10edb60b8dd144012e6f900d0cc5f
Hannele Ruiz <justlemoncl@gmail.com>
2018-09-07 13:20:57 -0300

    Added cheat to get the vehicle hash

--- PickYourTunes/Main.cs
+++ PickYourTunes/Main.cs
@@ -1,4 +1,5 @@
 ﻿using GTA;
+using GTA.Native;
 using System;
 
 namespace PickYourTunes
@@ -12,7 +13,17 @@ namespace PickYourTunes
 
         private void OnTick(object Sender, EventArgs Args)
         {
-
+            if (Function.Call<bool>(Hash._0x557E43C447E700A8, Game.GenerateHash("pythash")))
+            {
+                if (Game.Player.Character.CurrentVehicle == null)
+                {
+                    UI.Notify("You are not in a vehicle");
+                }
+                else
+                {
+                    UI.Notify(string.Format("Your vehicle hash is: {0}", Game.Player.Character.CurrentVehicle.Model.GetHashCode()));
+                }
+            }
         }
     }
 }
