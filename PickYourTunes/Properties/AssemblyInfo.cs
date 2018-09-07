cacb420efd0c3f16559e74d00ad68adcd2ca9ad7
Hannele Ruiz <justlemoncl@gmail.com>
2018-09-07 12:19:10 -0300

    Added basics

---
+++ PickYourTunes/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PickYourTunes")]
+[assembly: AssemblyDescription("Set the default radio station for a car directly from a configuration file")]
+[assembly: AssemblyCompany("justalemon")]
+[assembly: AssemblyProduct("PickYourTunes")]
+[assembly: AssemblyCopyright("© 2018 Hannele 'Lemon' Ruiz")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e7d4bf93-0a12-44f8-b61c-8f45c5bfaec8")]
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
---
+++ PickYourTunes/Script.cs
@@ -0,0 +1,6 @@
+﻿namespace PickYourTunes
+{
+    public class PickYourTunes
+    {
+    }
+}
