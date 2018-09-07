97afd6dd73ba001cfa27639064b2fb60bdc78726
Hannele Ruiz <justlemoncl@gmail.com>
2018-09-07 13:30:41 -0300

    Added another cheat to get the radio ID

--- PickYourTunes/Main.cs
+++ PickYourTunes/Main.cs
@@ -13,6 +13,7 @@ namespace PickYourTunes
 
         private void OnTick(object Sender, EventArgs Args)
         {
+            // Show the vehicle hash by using the cheat "pythash"
             if (Function.Call<bool>(Hash._0x557E43C447E700A8, Game.GenerateHash("pythash")))
             {
                 if (Game.Player.Character.CurrentVehicle == null)
@@ -22,6 +23,18 @@ namespace PickYourTunes
                 else
                 {
                     UI.Notify(string.Format("Your vehicle hash is: {0}", Game.Player.Character.CurrentVehicle.Model.GetHashCode()));
+                }
+            }
+            // Show the radio ID by using the "pytradio" cheat
+            if (Function.Call<bool>(Hash._0x557E43C447E700A8, Game.GenerateHash("pytradio")))
+            {
+                if (Game.Player.Character.CurrentVehicle == null)
+                {
+                    UI.Notify("You are not in a vehicle");
+                }
+                else
+                {
+                    UI.Notify(string.Format("The current radio ID is: {0}", Function.Call<int>(Hash.GET_PLAYER_RADIO_STATION_INDEX)));
                 }
             }
         }
