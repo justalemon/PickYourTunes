3dc36d2ee8c9a532dde367de125f88dfff5ace6d
Hannele Ruiz <justlemoncl@gmail.com>
2019-01-05 07:02:12 -0300

    Added file format for the default radios

---
+++ PickYourTunes/Items/Default.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PickYourTunes.Items
+{
+    public class Default
+    {
+        /// <summary>
+        /// The Model Hash for the Vehicle.
+        /// </summary>
+        [JsonProperty("hash")]
+        public int Hash { get; set; }
+        /// <summary>
+        /// The UUID's for the default radios.
+        /// </summary>
+        [JsonProperty("radios")]
+        public List<string> Radios { get; set; }
+    }
+}
